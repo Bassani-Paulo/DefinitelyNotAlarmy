90301f723e1527d2148a75ddc70cfdd772f206d0
Vitor Moriya <vitorjmoriya@gmail.com>
2021-06-07 01:19:23 -0300

    Adicionado Scroll na tela de Feed e adicionado as noticias recentes

--- DefinitelyNotAlarmy/DefinitelyNotAlarmy/FeedViewController.swift
+++ DefinitelyNotAlarmy/DefinitelyNotAlarmy/FeedViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 import SwiftUI
 
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UIScrollViewDelegate {
+    
     let feedView = FeedView()
+    let scrollView = UIScrollView()
+    
     override func viewDidLoad() {
-        navigationController?.setNavigationBarHidden(true, animated: false)
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .systemGray4
-        
-       
-        view.addSubview(feedView)
+        setupScrollView()
+        self.view.backgroundColor = .white
+    }
+
+    func setupScrollView(){
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         feedView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.scrollView.contentSize = CGSize(width: 390, height: 1200)
+        view.addSubview(scrollView)
+        scrollView.addSubview(feedView)
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        scrollView.frame = view.bounds
+        feedView.frame = CGRect(x: 0, y: 0, width: self.scrollView.contentSize.width, height: self.scrollView.contentSize.height)
     }
 }
 
@@ -30,7 +44,7 @@ class FeedView: UIView {
     let horoscopeView = HoroscopeView()
     let newsView = NewsView()
     override init(frame: CGRect) {
-        super.init(frame: frame)
+        super.init(frame: CGRect(x: 0, y: 0, width: 390, height: 2000))
         addSubview(weatherView)
         addSubview(horoscopeView)
         let adView = UIImageView(image: UIImage(named: "ad"))
@@ -40,14 +54,13 @@ class FeedView: UIView {
         horoscopeView.translatesAutoresizingMaskIntoConstraints = false
         adView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            weatherView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: -70),
+            weatherView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 0),
             weatherView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: -10),
             weatherView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: 400),
             weatherView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor, constant: 420),
             horoscopeView.topAnchor.constraint(equalTo: weatherView.bottomAnchor, constant: -140),
             horoscopeView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: -10),
             adView.topAnchor.constraint(equalTo: horoscopeView.bottomAnchor, constant: 140),
-            adView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 45)
         ])
     }
 
@@ -55,11 +68,9 @@ class FeedView: UIView {
         super.init(coder: coder)
 
     }
-
-    
 }
 
-class WeatherView: UIView{
+class WeatherView: UIView {
     var temperature:Float = 0
     
     let weatherIcon = UIImageView(image: UIImage(named: "SunnyCloud"))
@@ -288,13 +299,150 @@ class HoroscopeView: UIView {
 }
 
 class NewsView: UIView{
+    let newsTitleOne = UILabel()
+    let newsOneTimeDiff = UILabel()
+    let newsTitleTwo = UILabel()
+    let newsTwoTimeDiff = UILabel()
+    let newsTitleThree = UILabel()
+    let newsThreeTimeDiff = UILabel()
+    let newsTitleFour = UILabel()
+    let newsFourTimeDiff = UILabel()
     override init(frame: CGRect) {
-        super.init(frame: frame)
+        super.init(frame: CGRect(x: 0, y: 690, width: 390, height: 500))
+        self.backgroundColor = .white
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        titleLabel.text = "News"
+        titleLabel.numberOfLines = 0
+        addSubview(titleLabel)
+        
+        
+        newsTitleOne.translatesAutoresizingMaskIntoConstraints = false
+        newsTitleOne.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        newsTitleOne.text = "Placeholder"
+        newsTitleOne.numberOfLines = 0
+        addSubview(newsTitleOne)
+        
+        
+        newsOneTimeDiff.translatesAutoresizingMaskIntoConstraints = false
+        newsOneTimeDiff.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        newsOneTimeDiff.textColor = .systemGray3
+        newsOneTimeDiff.text = "Placeholder"
+        newsOneTimeDiff.numberOfLines = 0
+        addSubview(newsOneTimeDiff)
+        
+        newsTitleTwo.translatesAutoresizingMaskIntoConstraints = false
+        newsTitleTwo.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        newsTitleTwo.text = "Placeholder"
+        newsTitleTwo.numberOfLines = 0
+        addSubview(newsTitleTwo)
+        
+        
+        newsTwoTimeDiff.translatesAutoresizingMaskIntoConstraints = false
+        newsTwoTimeDiff.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        newsTwoTimeDiff.textColor = .systemGray3
+        newsTwoTimeDiff.text = "Placeholder"
+        newsTwoTimeDiff.numberOfLines = 0
+        addSubview(newsTwoTimeDiff)
+        
+        newsTitleThree.translatesAutoresizingMaskIntoConstraints = false
+        newsTitleThree.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        newsTitleThree.text = "Placeholder"
+        newsTitleThree.numberOfLines = 0
+        addSubview(newsTitleThree)
+        
+        
+        newsThreeTimeDiff.translatesAutoresizingMaskIntoConstraints = false
+        newsThreeTimeDiff.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        newsThreeTimeDiff.textColor = .systemGray3
+        newsThreeTimeDiff.text = "Placeholder"
+        newsThreeTimeDiff.numberOfLines = 0
+        addSubview(newsThreeTimeDiff)
+        
+        newsTitleFour.translatesAutoresizingMaskIntoConstraints = false
+        newsTitleFour.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        newsTitleFour.text = "Placeholder"
+        newsTitleFour.numberOfLines = 0
+        addSubview(newsTitleFour)
+        
+        
+        newsFourTimeDiff.translatesAutoresizingMaskIntoConstraints = false
+        newsFourTimeDiff.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        newsFourTimeDiff.textColor = .systemGray3
+        newsFourTimeDiff.text = "Placeholder"
+        newsFourTimeDiff.numberOfLines = 0
+        addSubview(newsFourTimeDiff)
+        
+        
+        fetchNews(from: URL(string: "https://newsapi.org/v2/top-headlines?apiKey=f468d582d4e445cb828b8334bc29daf6&country=br")!)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 19),
+            titleLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            newsTitleOne.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 18),
+            newsTitleOne.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            newsTitleOne.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: 15),
+            newsOneTimeDiff.topAnchor.constraint(equalTo: newsTitleOne.bottomAnchor, constant: 6),
+            newsOneTimeDiff.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            newsTitleTwo.topAnchor.constraint(equalTo: newsOneTimeDiff.bottomAnchor, constant: 36),
+            newsTitleTwo.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            newsTitleTwo.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: 15),
+            newsTwoTimeDiff.topAnchor.constraint(equalTo: newsTitleTwo.bottomAnchor, constant: 6),
+            newsTwoTimeDiff.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            
+            newsTitleThree.topAnchor.constraint(equalTo: newsTwoTimeDiff.bottomAnchor, constant: 36),
+            newsTitleThree.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            newsTitleThree.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: 15),
+            newsThreeTimeDiff.topAnchor.constraint(equalTo: newsTitleThree.bottomAnchor, constant: 6),
+            newsThreeTimeDiff.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            
+            newsTitleFour.topAnchor.constraint(equalTo: newsThreeTimeDiff.bottomAnchor, constant: 36),
+            newsTitleFour.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15),
+            newsTitleFour.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: 15),
+            newsFourTimeDiff.topAnchor.constraint(equalTo: newsTitleFour.bottomAnchor, constant: 6),
+            newsFourTimeDiff.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 15)
+        ])
+        
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         fatalError("init(coder:) has not been implemented")
+    }
+    func fetchNews(from url:URL) {
+        let dataTask = URLSession.shared.dataTask(with: url){ data, _, _ in
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async() {
+                do{
+                    let decoder = JSONDecoder()
+                    let newsResponse = try decoder.decode(NewsInfo.self, from: data)
+                    self.newsTitleOne.text = newsResponse.articles.first?.title
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                    var date = dateFormatter.date(from:newsResponse.articles.first!.publishedAt)!
+                    self.newsOneTimeDiff.text = "\(Calendar.current.dateComponents([.hour], from: date, to: Date()).hour ?? 0) hours ago"
+                    
+                    self.newsTitleTwo.text = newsResponse.articles[1].title
+                    date = dateFormatter.date(from:newsResponse.articles[1].publishedAt)!
+                    self.newsTwoTimeDiff.text = "\(Calendar.current.dateComponents([.hour], from: date, to: Date()).hour ?? 0) hours ago"
+                    
+                    self.newsTitleThree.text = newsResponse.articles[2].title
+                    date = dateFormatter.date(from:newsResponse.articles[2].publishedAt)!
+                    self.newsThreeTimeDiff.text = "\(Calendar.current.dateComponents([.hour], from: date, to: Date()).hour ?? 0) hours ago"
+                    
+                    self.newsTitleFour.text = newsResponse.articles[3].title
+                    date = dateFormatter.date(from:newsResponse.articles[3].publishedAt)!
+                    self.newsFourTimeDiff.text = "\(Calendar.current.dateComponents([.hour], from: date, to: Date()).hour ?? 0) hours ago"
+                    
+                    }catch{
+                    print("error")
+                }
+            }
+            
+        }
+        dataTask.resume()
     }
 }
 
@@ -320,6 +468,17 @@ enum weatherError: Error{
     case noDataAvaible
     case cannotProcessData
 }
+
+struct NewsInfo: Decodable{
+    var articles: [News]
+}
+
+struct News: Decodable{
+    var title: String
+    var publishedAt: String
+    
+}
+
 
 struct WeatherRequest{
     let resourceURL: URL
